56b72a8e972ad1e3be080c1ce176d5981bb75a41
pacharanero <marcusbaw@gmail.com>
2014-04-20 18:38:59 +0100

    scratch.py now calculates percentile from value

--- scratch.py
+++ scratch.py
@@ -1,14 +1,13 @@
 #!usr/bin/python
 import scipy.stats as st
 
-value = 81.99171445 #this should be the 50th percentile
+value = 80.72977321
 L = 0.941523967
 M = 86.45220101
 S = 0.040321528  
 
-z_score = (((value/M)**L)-1.0)/L*S
-print z_score
-
-print st.norm.ppf(z_score)
+z_score = (((value/M)**L)-1.0)/(L*S)
+print "z-score:" + str(z_score)
+print "percentile:" + str(st.norm.cdf(z_score))
 
   
