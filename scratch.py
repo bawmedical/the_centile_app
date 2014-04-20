3c9a7fd9e13f5d8a35471832f47b467d6bade368
pacharanero <marcusbaw@gmail.com>
2014-04-20 22:37:34 +0100

    scratch.py added command line function

--- scratch.py
+++ scratch.py
@@ -1,13 +1,24 @@
 #!usr/bin/python
 import scipy.stats as st
+import sys
 
-value = 80.72977321
+#grab command line args: (argv[0]=script name), height, weight, age, sex
+height = float(sys.argv[1]) #centimetres
+weight = float(sys.argv[2]) #kilograms
+age = float(sys.argv[3])    #months
+sex = float(sys.argv[4])    # "M" or "F"
+
+#lookup LMS values for that age and sex from dataset currently hardcoded for 1yr old male and height chart
+#TODO
 L = 0.941523967
 M = 86.45220101
-S = 0.040321528  
+S = 0.040321528
 
-z_score = (((value/M)**L)-1.0)/(L*S)
-print "z-score:" + str(z_score)
-print "percentile:" + str(st.norm.cdf(z_score))
+#calculate percentile of the offered values
+height_centile = st.norm.cdf( ( ((height/M)**L)-1.0) / (L*S) )
+#weight_centile = st.norm.cdf( ( ((weight/M)**L)-1.0) / (L*S) )
 
-  
+print "percentile:" + str(height_centile)
+
+#need to return a range of values for context - eg heights for 3rd, 5th, 10th, 50th....
+ 
