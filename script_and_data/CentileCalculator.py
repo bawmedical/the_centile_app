3eb84533c58dd912a8dd80ab9368a0e5f2a3040f
paruby <paruby@gmail.com>
2014-04-23 12:02:08 +0100

    Update CentileCalculator.py
    Added display of context centiles
    Centiles printed to 1dp
    Simplified evaluation of centile from Z score in lms_to_centile()

--- script_and_data/CentileCalculator.py
+++ script_and_data/CentileCalculator.py
@@ -2,6 +2,7 @@
 
 from math import * 
 from numpy import *
+import scipy.stats as st
 import sys
 
 def get_all_centiles(height,weight,age,sex):
@@ -24,8 +25,9 @@ def calculate_height_centile(height, age, sex):
 		i=i+1
 	
 	[L,M,S,p3,p5,p10,p25,p50,p75,p90,p95,p97] = get_lms_and_percentiles(heightData[i])
+	context = [p3,p5,p10,p25,p50,p75,p90,p95,p97]
 	heightCentile = lms_to_centile(height, L, M, S)
-	return heightCentile
+	return heightCentile, context
 
 
 def calculate_weight_centile(weight, age, sex):
@@ -40,8 +42,9 @@ def calculate_weight_centile(weight, age, sex):
 		i=i+1
 	
 	[L,M,S,p3,p5,p10,p25,p50,p75,p90,p95,p97] = get_lms_and_percentiles(weightData[i])
+	context = [p3,p5,p10,p25,p50,p75,p90,p95,p97]
 	weightCentile = lms_to_centile(weight, L, M, S)
-	return weightCentile
+	return weightCentile, context
 	
 
 def calculate_bmi_centile(height, weight, age, sex):
@@ -57,10 +60,10 @@ def calculate_bmi_centile(height, weight, age, sex):
 		i=i+1
 	
 	[L,M,S,p3,p5,p10,p25,p50,p75,p90,p95,p97] = get_lms_and_percentiles(bmiData[i])
-	
+	context = [p3,p5,p10,p25,p50,p75,p90,p95,p97]
 	bmi = weight/((height/100)**2)
 	bmiCentile = lms_to_centile(bmi, L, M, S)
-	return bmiCentile
+	return bmiCentile, context
 
 	
 def lms_to_centile(X, L, M, S):
@@ -71,24 +74,23 @@ def lms_to_centile(X, L, M, S):
 		Z = ln(X/M)/S
 	else:
 		Z = (((X/M)**L)-1)/(L*S)
-	centile = (1+erf(Z/sqrt(2)))/2
+	centile = st.norm.cdf(Z)
 	return centile
 	
 def get_lms_and_percentiles(array):
-	L = array[2]
-	M = array[3]
-	S = array[4]
-	p3 = array[5]
-	p5 = array[6]
-	p10 = array[7]
-	p25 = array[8]
-	p50 = array[9]
-	p75 = array[10]
-	p90 = array[11]
-	p95 = array[12]
-	p97 = array[13]
+	[L, M, S, p3, p5, p10, p25, p50, p75, p90, p95, p97] = array[2:14]
 	return [L, M, S, p3, p5, p10, p25, p50, p75, p90, p95, p97]
 
+def display_context(percentiles):
+	print "3%: " + str(percentiles[0])
+	print "5%: " + str(percentiles[1])
+	print "10%: " + str(percentiles[2])
+	print "25%: " + str(percentiles[3])
+	print "50%: " + str(percentiles[4])
+	print "75%: " + str(percentiles[5])
+	print "90%: " + str(percentiles[6])
+	print "95%: " + str(percentiles[7])
+	print "97%: " + str(percentiles[8])
 
 
 #grab command line args: (argv[0]=script name), height, weight, age, sex
@@ -97,7 +99,25 @@ weight = float(sys.argv[2]) #kilograms
 age = float(sys.argv[3])    #months
 sex = str(sys.argv[4])    # "M" or "F"
 
+[heightCentile, heightContext] = calculate_height_centile(height, age, sex)
+[weightCentile, weightContext] = calculate_weight_centile(weight, age, sex)
+[bmiCentile, bmiContext] = calculate_bmi_centile(height, weight, age, sex)
 
-print "Height percentile is: " + str(calculate_height_centile(height, age, sex))
-print "Weight percentile is: " + str(calculate_weight_centile(weight, age, sex))
-print "BMI percentile is: " + str(calculate_bmi_centile(height, weight, age, sex))
+print("Height percentile is: %.1f" % (heightCentile*100) +"%")
+print("Weight percentile is: %.1f" % (weightCentile*100) +"%")
+print("BMI percentile is: %.1f" % (bmiCentile*100) +"%")
+
+print"-------------------"
+
+print "For context, the height centiles are: "
+display_context(heightContext)
+
+print"-------------------"
+
+print "For context, the weight centiles are: "
+display_context(weightContext)
+
+print"-------------------"
+
+print "For context, the BMI centiles are: "
+display_context(bmiContext)
